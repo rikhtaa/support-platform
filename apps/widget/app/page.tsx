01bdd914d7cc8c6be425088d98889eaf7d65e66a
Rekhta Menahil <rekhta674@gmail.com>
2026-04-06 14:07:02 +0500

    feat: add turborepo

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,6 +1,8 @@
-import { Button } from "@workspace/ui/components/button"
 import { add } from "@workspace/math/add"
-export default function Page() {  
+import { Button } from "@workspace/ui/components/button"
+import {Input} from "@workspace/ui/components/input"
+
+export default function Page() {
   return (
     <div className="flex min-h-svh p-6">
       <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
@@ -9,7 +11,8 @@ export default function Page() {
           <p>You may now add components and start building.</p>
           <p>We&apos;ve already added the button component for you.</p>
           <Button className="mt-2">Button</Button>
-          <p>{add(2, 3)}</p>
+          {add(2, 2)}
+          <Input placeholder="Input component" className="mt-2" />
         </div>
         <div className="text-muted-foreground font-mono text-xs">
           (Press <kbd>d</kbd> to toggle dark mode)
---
+++ packages/ui/globals.css.d.ts
@@ -0,0 +1,1 @@
+declare module "@workspace/ui/globals.css";
