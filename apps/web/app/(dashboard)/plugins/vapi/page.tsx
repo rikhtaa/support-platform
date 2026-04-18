c0d37dd78b60b6d6b301059483ab53f1cfc6560d
Rekhta Menahil <rekhta674@gmail.com>
2026-04-18 13:46:46 +0500

    add dashboard layout

---
+++ apps/web/app/(dashboard)/plugins/vapi/page.tsx
@@ -0,0 +1,9 @@
+const Page = () => {
+  return (
+    <div>
+        Vapi Plugin
+    </div>
+  )
+}
+
+export default Page
---
+++ apps/web/app/layout.tsx
@@ -0,0 +1,37 @@
+import { Geist, Geist_Mono } from "next/font/google"
+import { ClerkProvider } from "@clerk/nextjs";
+import "@workspace/ui/styles/globals.css"
+import { ThemeProvider } from "@/components/theme-provider"
+import { cn } from "@workspace/ui/lib/utils";
+import { TooltipProvider } from "@workspace/ui/components/tooltip";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'})
+
+const fontMono = Geist_Mono({
+  subsets: ["latin"],
+  variable: "--font-mono",
+})
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html
+      lang="en"
+      suppressHydrationWarning
+      className={cn("antialiased", fontMono.variable, "font-sans", geist.variable)}
+    >
+      <body>
+        <ClerkProvider>
+          <ThemeProvider>
+            <TooltipProvider>
+            {children}
+            </TooltipProvider>
+            </ThemeProvider>
+        </ClerkProvider>
+      </body>
+    </html>
+  )
+}
