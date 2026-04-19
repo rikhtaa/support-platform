51b81bafc0f02e980e9f3accb9236454fa0899d5
Rekhta Menahil <rekhta674@gmail.com>
2026-04-20 03:33:41 +0500

    add widget session

--- apps/widget/modules/widget/ui/components/widget-header.tsx
+++ apps/widget/modules/widget/ui/components/widget-header.tsx
@@ -2,12 +2,15 @@ import { cn } from "@workspace/ui/lib/utils"
 
 export const WidgetHeader = ({
     children,
+    className
 }: {
-   children: React.ReactNode
+   children: React.ReactNode,
+    className?: string,
 }) => {
     return (
         <header className={cn(
-            "bg-gradient-to-b from-primary to-[#0b63f3] p-4 text-primary-foreground"
+            "bg-gradient-to-b from-primary to-[#0b63f3] p-4 text-primary-foreground",
+            className
         )}>
          {children}
         </header>
