9adf21b73674334835fbef36d5972f919ce88aa6
Rekhta Menahil <rekhta674@gmail.com>
2026-04-28 11:08:17 +0500

    add widget inbox

--- apps/widget/modules/widget/ui/screens/widget-selection-screen.tsx
+++ apps/widget/modules/widget/ui/screens/widget-selection-screen.tsx
@@ -8,6 +8,7 @@ import { Button } from "@workspace/ui/components/button"
 import { useMutation } from "convex/react"
 import { api } from "@workspace/backend/_generated/api"
 import { useState } from "react"
+import { WidgetFooter } from "../components/widget-footer";
 
 
 export const WidgetSelectionScreen = () => {
@@ -71,6 +72,7 @@ export const WidgetSelectionScreen = () => {
             <ChevronRightIcon/>
           </Button>
          </div>
+         <WidgetFooter/>
         </>
     )
 }
