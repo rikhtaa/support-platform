b7bd3195d4750a46167a966658a1f1cd95cedfd8
Rekhta Menahil <rekhta674@gmail.com>
2026-05-30 21:03:33 +0500

    fix widget url

--- apps/embed/config.ts
+++ apps/embed/config.ts
@@ -1,6 +1,6 @@
 export const EMBED_CONFIG = {
   WIDGET_URL: import.meta.env.PROD
-  ? "https://support-platform-widget-lime.vercel.app"
+  ? "https://support-platform-widget-lime.vercel.app/widget"
   : "http://localhost:3001",
   DEFAULT_ORG_ID: "org_3CZzYyYx1HIAbkfLONcTwxDRhsW",
   DEFAULT_POSITION: "bottom-right" as const,
