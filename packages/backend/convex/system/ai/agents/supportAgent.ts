bf057ae6a4687a19d2fd51411e0c03c408cf3d6e
Rekhta Menahil <rekhta674@gmail.com>
2026-06-10 10:10:44 +0500

    replace groq with gemini

--- packages/backend/convex/system/ai/agents/supportAgent.ts
+++ packages/backend/convex/system/ai/agents/supportAgent.ts
@@ -1,14 +1,14 @@
-import { groq } from "@ai-sdk/groq";
 import { Agent } from "@convex-dev/agent";
 import { components } from "../../../_generated/api"
 import { search } from "../tools/search";
 import { escalateConversation } from "../tools/escalateConversation";
 import { resolveConversation } from "../tools/resolveConversation";
 import { SUPPORT_AGENT_PROMPT } from "../constants";
+import { google } from "@ai-sdk/google";
 
 export const supportAgent = new Agent(components.agent, {
     name: "Support Agent",
-    languageModel: groq("llama-3.3-70b-versatile"),
+    languageModel: google("gemini-2.0-flash"),
     tools: { search, escalateConversation, resolveConversation },
     instructions: SUPPORT_AGENT_PROMPT,
 })
