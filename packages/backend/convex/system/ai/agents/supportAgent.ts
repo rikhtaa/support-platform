50052fdfe54beeaa9b9bd07a2491b4c0d5d50299
Rekhta Menahil <rekhta674@gmail.com>
2026-06-10 14:06:22 +0500

    fix support Agent

--- packages/backend/convex/system/ai/agents/supportAgent.ts
+++ packages/backend/convex/system/ai/agents/supportAgent.ts
@@ -1,14 +1,14 @@
+import { groq } from "@ai-sdk/groq";
 import { Agent } from "@convex-dev/agent";
 import { components } from "../../../_generated/api"
 import { search } from "../tools/search";
 import { escalateConversation } from "../tools/escalateConversation";
 import { resolveConversation } from "../tools/resolveConversation";
 import { SUPPORT_AGENT_PROMPT } from "../constants";
-import { google } from "@ai-sdk/google";
 
 export const supportAgent = new Agent(components.agent, {
     name: "Support Agent",
-    languageModel: google("gemini-2.0-flash"),
+    languageModel: groq("llama-3.3-70b-versatile"),
     tools: { search, escalateConversation, resolveConversation },
     instructions: SUPPORT_AGENT_PROMPT,
 })
